0d074526db66267d068dc2f059ab1a75ee089ac5
Mortimer <MortimerPavlitski@gmail.com>
2023-08-15 13:17:00 +0100

    update flex.log

--- maintain_github_flex.sh
+++ maintain_github_flex.sh
@@ -6,4 +6,6 @@ cd ~/Documents/Code/githubflex/
 date_time=$(date -u)
 echo "Maintained flex at $date_time" > flex.log
 
-#push repo back as new comit
+git add .             
+git commit -m "update flex.log"
+git push -u origin
